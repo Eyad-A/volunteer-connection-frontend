0d918f932edf41025770959029c91ddcac5ff0e2
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-07-12 11:55:29 -0500

    Add routes-nav components

---
+++ src/routes-nav/Navigation.js
@@ -0,0 +1,90 @@
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import UserContext from "../auth/UserContext";
+import "./Navigation.css"
+
+/**
+ * Nav bar that shows up on top of every page
+ * If the user/company is logged in, will show links to 
+ * companies/profile. Otherwise, will show 
+ * login/signup links. 
+ */
+
+function Navigation({ logout }) {
+
+  const { currentUser } = useContext(UserContext);
+
+  function loggedInNav() {
+    return (
+      <div className="container-fluid">
+        <Link className="navbar-brand link-dark" to="/">
+          Volunteer Connection
+        </Link>
+        <div className="justify-content-end">
+          <ul className="navbar-nav ml-auto justify-content-end">
+            <li className="nav-item mr-4">
+              <NavLink className="nav-link" to="/companies">
+                Companies
+              </NavLink>
+            </li>
+            <li className="nav-item mr-4">
+              <NavLink className="nav-link" to="/connections">
+                connections 
+              </NavLink>
+            </li>
+            <li className="nav-item mr-4">
+              <NavLink className="nav-link" to="/profile">
+                Profile
+              </NavLink>
+            </li>
+            <li className="nav-item mr-4">
+              <NavLink className="nav-link" to="/" onClick={logout}>
+                Logout
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+    );
+  }
+
+  function loggedOutNav() {
+    return (
+      <div className="container-fluid">
+        <Link className="navbar-brand link-dark" to="/">
+          Volunteer Connection 
+        </Link>
+        <div className="justify-content-end">
+          <ul className="navbar-nav ml-auto justify-content-end">
+            <li className="nav-item mr-4">
+              <NavLink className="nav-link" to="/companies">
+                Companies
+              </NavLink>
+            </li>            
+            <li className="navbar-item mr-4">
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </li>
+            <li className="navbar-item mr-4">
+              <NavLink className="nav-link" to="/signup">
+                Signup
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    );
+  }
+
+  return (
+    <nav className="Navigation navbar navbar-expand-md">
+      {currentUser ? loggedInNav() : loggedOutNav()}
+    </nav>
+  );
+}
+
+export default Navigation;
---
+++ src/routes-nav/Routes.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Homepage from "../homepage/Homepage";
+import UserLoginForm from "../auth/UserLoginForm";
+import CompanyLoginForm from "../auth/CompanyLoginForm";
+import UserSignupForm from "../auth/UserSignupForm";
+import CompanySignupForm from "../auth/CompanySignupForm";
+import CompanyList from "../companies/CompanyList";
+import CompanyDetail from "../companies/CompanyDetail";
+import UserProfileForm from "../profiles/UserProfileForm";
+import CompanyProfileForm from "../profiles/CompanyProfileForm";
+import PrivateRoute from "./PrivateRoute";
+
+function Routes({ loginUser, signupUser, loginCompany, signupCompany }) {
+  return (
+    <div className="">
+      <Switch>
+
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+
+        <Route exact path="/login-user">
+          <UserLoginForm loginUser={loginUser} />
+        </Route>
+
+        <Route exact path="/signup-user">
+          <UserSignupForm signupUser={signupUser} />
+        </Route>
+
+        <Route exact path="/login-company">
+          <CompanyLoginForm loginCompany={loginCompany} />
+        </Route>
+
+        <Route exact path="/signup-company">
+          <CompanySignupForm signupCompany={signupCompany} />
+        </Route>
+
+        <Route exact path="/companies">
+          <CompanyList />
+        </Route>        
+
+        <Route exact path="/companies/:companyHandle">
+          <CompanyDetail />
+        </Route>
+        
+        <PrivateRoute exact path="/profile-user">
+          <UserProfileForm />
+        </PrivateRoute>
+
+        <PrivateRoute exact path="/profile-company">
+          <CompanyProfileForm />
+        </PrivateRoute>
+
+        <Redirect to="/" />
+      </Switch>
+    </div>
+  );
+}
+
+export default Routes;
