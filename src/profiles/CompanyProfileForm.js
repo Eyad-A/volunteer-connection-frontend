ffa7126b7f4b36daa169dd88bedd9eda20a03157
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-07-12 11:27:43 -0500

    Add profile forms

---
+++ src/profiles/CompanyProfileForm.js
@@ -0,0 +1,195 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../auth/UserContext";
+import VolunteerApi from "../api/api";
+import Alert from "../common/Alert";
+
+/**
+ * Company Profile Form 
+ * Renders a form to edit the user's profile 
+ * Routed at /profile 
+ */
+
+function CompanyProfileForm() {
+  const { currentCompany, setCurrentCompany } = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    companyName: currentCompany.companyName,
+    country: currentCompany.country,
+    numEmployees: currentCompany.numEmployees,
+    shortDescription: currentCompany.shortDescription,
+    longDescription: currentCompany.longDescription,
+    websiteUrl: currentCompany.websiteUrl,
+    logoUrl: currentCompany.logoUrl,
+    mainImageUrl: currentCompany.mainImageUrl,
+    lookingFor: currentCompany.lookingFor,
+    password: "",
+  });
+  const [formErrors, setFormErrors] = useState([]);
+  const [saveConfirmed, setSaveConfirmed] = useState(false);
+
+  // Handle form submit 
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+
+    let profileData = {
+      companyName: formData.companyName,
+      country: formData.country,
+      numEmployees: formData.numEmployees,
+      shortDescription: formData.shortDescription,
+      longDescription: formData.longDescription,
+      websiteUrl: formData.websiteUrl,
+      logoUrl: formData.logoUrl,
+      mainImageUrl: formData.mainImageUrl,
+      lookingFor: formData.lookingFor,
+      password: formData.password,
+    };
+
+    let companyHandle = formData.companyHandle;
+    let updatedCompany;
+
+    try {
+      updatedCompany = await VolunteerApi.saveCompanyProfile(companyHandle, profileData);
+    } catch (err) {
+      setFormErrors(err);
+      return;
+    }
+
+    setFormData(f => ({ ...f, password: "" }));
+    setFormErrors([]);
+    setSaveConfirmed(true);
+
+    setCurrentCompany(updatedCompany);
+  }
+
+  // Handle form change 
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData(f => ({
+      ...f,
+      [name]: value,
+    }));
+    setFormErrors([]);
+  }
+
+  return (
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-lg-2">
+
+        </div>
+        <div className="col-lg-10">
+          <h1>Company Profile Form</h1>
+          <div className="my-3">
+            <form>
+              <div className="col-lg-12 my-2">
+                <label>Company Handle</label>               
+                <p>{formData.companyHandle}</p>
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Country</label>
+                <input
+                  name="country"
+                  className="form-control"                  
+                  value={formData.country}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Number of employees</label>            
+                <input
+                  name="numEmployees"
+                  className="form-control"
+                  value={formData.numEmployees}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Short Description</label>
+                <input
+                  name="shortDescription"
+                  className="form-control"
+                  value={formData.shortDescription}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Long Description</label>
+                <input
+                  name="longDescription"
+                  className="form-control"
+                  value={formData.longDescription}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>website URL</label>
+                <input
+                  name="websiteUrl"
+                  className="form-control"
+                  value={formData.websiteUrl}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Logo URL</label>
+                <input
+                  name="logoUrl"
+                  className="form-control"
+                  value={formData.logoUrl}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Main image URL</label>
+                <input
+                  name="mainImageUrl"
+                  className="form-control"
+                  value={formData.mainImageUrl}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Looking for?</label>
+                <input
+                  name="lookingFor"
+                  className="form-control"
+                  value={formData.lookingFor}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Confirm password to make changes</label>
+                <input
+                  name="password"
+                  className="form-control"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </div>
+
+              {formErrors.length ?
+                <Alert type="danger" message={formErrors} />
+                : null
+              }
+
+              {saveConfirmed ?
+                <Alert type="success" messages={["Update successful"]} />
+                : null
+              }
+
+              <button className="btn btn-lg btn-primary my-3" onClick={handleSubmit}>
+                Save
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="col-lg-2">
+
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default ProfileForm;
---
+++ src/profiles/UserProfileForm.js
@@ -0,0 +1,150 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../auth/UserContext";
+import VolunteerApi from "../api/api";
+import Alert from "../common/Alert";
+
+/**
+ * User Profile Form 
+ * Renders a form to edit the user's profile 
+ * Routed at /profile 
+ */
+
+function UserProfileForm() {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    firstName: currentUser.firstName,
+    lastName: currentUser.lastName,
+    email: currentUser.email,
+    skill: currentUser.skill,
+    username: currentUser.username,
+    password: "",
+  });
+  const [formErrors, setFormErrors] = useState([]);
+  const [saveConfirmed, setSaveConfirmed] = useState(false);
+
+  // Handle form submit 
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+
+    let profileData = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      skill: formData.skill,
+      password: formData.password,
+    };
+
+    let username = formData.username;
+    let updatedUser;
+
+    try {
+      updatedUser = await VolunteerApi.saveUserProfile(username, profileData);
+    } catch (err) {
+      setFormErrors(err);
+      return;
+    }
+
+    setFormData(f => ({ ...f, password: "" }));
+    setFormErrors([]);
+    setSaveConfirmed(true);
+
+    setCurrentUser(updatedUser);
+  }
+
+  // Handle form change 
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData(f => ({
+      ...f,
+      [name]: value,
+    }));
+    setFormErrors([]);
+  }
+
+  return (
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-lg-2">
+
+        </div>
+        <div className="col-lg-10">
+          <h1>User Profile Form</h1>
+          <div className="my-3">
+            <form>
+              <div className="col-lg-12 my-2">
+                <label>Username</label>               
+                <p>{formData.username}</p>
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>First name</label>                
+                <input
+                  name="firstName"
+                  className="form-control"                  
+                  value={formData.firstName}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Last name</label>            
+                <input
+                  name="lastName"
+                  className="form-control"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Skill</label>
+                <input
+                  name="skill"
+                  className="form-control"
+                  value={formData.skill}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Email</label>
+                <input
+                  name="email"
+                  className="form-control"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-12 my-2">
+                <label>Confirm password to make changes</label>
+                <input
+                  name="password"
+                  className="form-control"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </div>
+
+              {formErrors.length ?
+                <Alert type="danger" message={formErrors} />
+                : null
+              }
+
+              {saveConfirmed ?
+                <Alert type="success" messages={["Update successful"]} />
+                : null
+              }
+
+              <button className="btn btn-lg btn-primary my-3" onClick={handleSubmit}>
+                Save
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="col-lg-2">
+
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default UserProfileForm;
