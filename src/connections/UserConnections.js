ab6c1a60e5086f1d17def817a3ee9e0f95a46d17
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-07-14 16:44:14 -0500

    Add user connections page

--- src/connections/UserConnections.js
+++ src/connections/UserConnections.js
@@ -1,25 +1,48 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import UserContext from "../auth/UserContext";
 import VolunteerApi from "../api/api";
+import CompanyCard from "../companies/CompanyCard";
 
 function UserConnections() {
   const { currentUser } = useContext(UserContext);
   const connections = currentUser.connections;
+  const [companies, setCompany] = useState([]);
+
+  if (connections) {
+    for (const connection in connections) {
+      useEffect(function getCompanyDetail() {
+        async function getCompany() {
+          setCompany(await VolunteerApi.getCurrentCompany(connection));
+        }
+        getCompany();
+      }, [connection]);
+    }    
+  }
 
   return (
     <div class="container">
       <div class="row">
         <div class="col-lg-9">
-          {connections.length 
-          ? (
-            <div>
-              {connections.map(c => (
-                c
-              ))}
-            </div>  
-          ) : (
-            <p>You have no connections</p>
-          )}
+          {companies.length
+            ? (
+              <div>
+                {companies.map(company => (
+                  <CompanyCard
+                  key={company.companyHandle}
+                  companyHandle={company.companyHandle}
+                  companyName={company.companyName}
+                  country={company.country}
+                  numEmployees={c.numEmployees}
+                  shortDescription={c.shortDescription}                                   
+                />
+                )
+                   
+                )}
+
+              </div>
+            ) : (
+              <p>You have no connections</p>
+            )}
         </div>
       </div>
     </div>
